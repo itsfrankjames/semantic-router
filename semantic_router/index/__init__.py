c7f785e3256a7658f6d80f90d02522ef1b0f5725
Lawson Taylor <lawson.j.taylor@gmail.com>
2024-06-28 11:38:36 +0800

    Export PostgresIndex from .index

--- semantic_router/index/__init__.py
+++ semantic_router/index/__init__.py
@@ -2,10 +2,12 @@ from semantic_router.index.base import BaseIndex
 from semantic_router.index.local import LocalIndex
 from semantic_router.index.pinecone import PineconeIndex
 from semantic_router.index.qdrant import QdrantIndex
+from semantic_router.index.postgres import PostgresIndex
 
 __all__ = [
     "BaseIndex",
     "LocalIndex",
     "QdrantIndex",
     "PineconeIndex",
+    "PostgresIndex"
 ]
